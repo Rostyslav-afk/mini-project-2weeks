4caf8fe49a1fd18a646e0378ea7f9cdd4cee52ac
veazerrDev <110056857+Rostyslav-afk@users.noreply.github.com>
2025-11-10 17:37:33 +0000

    full

--- src/app.js
+++ src/app.js
@@ -5,14 +5,78 @@ import {
 import * as PNotifyMobile from "../node_modules/@pnotify/mobile/dist/PNotifyMobile.js";
 import "@pnotify/core/dist/BrightTheme.css";
 defaultModules.set(PNotifyMobile, {});
+import debounce from "lodash.debounce";
+import fetchCountries from "./fetchCountries.js";
+
+const appInput = document.querySelector(".app-input");
+const appList = document.querySelector(".app-list");
+const appDiv = document.querySelector(".app-div");
+
+appInput.addEventListener("input", debounce(searchCountry, 500));
+
+function searchCountry(evt) {
+  const countryName = evt.target.value.trim();
+  appList.innerHTML = "";
+  appDiv.innerHTML = "";
+  // Res є моїм масивом з країнами
+  fetchCountries(countryName).then((res) => {
+    if (res.length > 10) {
+      appList.innerHTML = "";
+      appDiv.innerHTML = "";
+      errorMessage();
+      return;
+    }
+    if (res.length > 1 && res.length <= 10) {
+      appList.innerHTML = "";
+      appDiv.innerHTML = "";
+      const countryName = res
+        .map((country) => `<li class="app-item">${country.name.common}</li>`)
+        .join("");
+      appList.innerHTML = countryName;
+      return;
+    }
+    if (res.length === 1) {
+      // capital - столиця, languages - мови, population - популяція, flags.png - прапор в пнг
+      appList.innerHTML = "";
+      appDiv.innerHTML = "";
+      const countryInfo = res.map(
+        ({ name, capital, population, flags, languages }) => {
+          const languagesArray = Object.values(languages);
+          return (appDiv.innerHTML = `<h1 class="app-cname">${name.common}</h1>
+            <div class="app-box">
+                <div class="app-cinfo">
+                    <h3 class="app-subtitle">Capital: <span
+                            class="app-subtitle-info">${capital}</span></h3>
+                    <h3 class="app-subtitle">Population: <span
+                            class="app-subtitle-info">${population}</span></h3>
+                    <h3 class="app-subtitle">Languages:
+                        <ul class="app-clanguages">
+                           ${languagesArray
+                             .map(
+                               (language) => `<li class="clanguages-item">
+                                <h3 class="clanguages-subtitle">${language}</h3>
+                            </li>`
+                             )
+                             .join("")}
+                        </ul>
+                    </h3>
+                </div>
+                <div class="app-cimage">
+                    <img src="${flags.png}" alt="Country Flag."
+                        class="app-cimg" width="400" height="300">
+                </div>
+            </div>`).join("");
+        }
+      );
+      return;
+    }
+  });
+}
 
 function errorMessage() {
   error({
     title: "Oh No!",
     text: "Write the name of the country more precisely!",
-    delay: 3000
+    delay: 1000,
   });
 }
-
-errorMessage()
-
